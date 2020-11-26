b0d3f110cfec370a1390d2a346673a085becd82d
remote-torrent-6747 <abbyc@sky.com>
2020-11-26 14:57:14 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,24 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(input)
+  input.to_i - 1
+end
+
+def move(board, index, character = "X")
+  board[index] = character
+end
+
+def valid_move? (board, index)
+  if index > 8
+    return false
+  elsif board[index] == "X" || board[index] == "O"
+    return false
+  elsif board[index] == " " || board[index] == "" || board[index] == nil
+    return true
+end
